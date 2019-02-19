7cbb93804b88c501d17b0c38f51265d63d73cd64
PaolaMedinaB <pmmedinab@unal.edu.co>
2019-02-19 02:13:58 +0100

    Update punto_1.sql

--- punto_1.sql
+++ punto_1.sql
@@ -1,2 +1,7 @@
+
 SELECT departments.location_id, locations.street_address, locations.city, locations.state_province, countries.country_name
-FROM departments, locations, countries
+FROM departments
+INNER JOIN locations
+ON departments.LOCATION_ID = locations.LOCATION_ID
+INNER JOIN countries
+ON  locations.COUNTRY_ID = countries.COUNTRY_ID 
