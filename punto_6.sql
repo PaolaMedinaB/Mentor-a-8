e1dc5196f39a2fcbeb1b7f4d0422155583467efb
PaolaMedinaB <pmmedinab@unal.edu.co>
2019-02-19 02:47:53 +0100

    Update punto_6.sql

--- punto_6.sql
+++ punto_6.sql
@@ -1,4 +1,6 @@
 SELECT  COUNT(employee_id), department_name
-FROM employees, departments
+FROM employees
+INNER JOIN departments
+ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
 GROUP BY department_name
 
