4c2a9431188c37e028efd7b2956c1722df92cbb8
PaolaMedinaB <pmmedinab@unal.edu.co>
2019-02-19 02:27:52 +0100

    Update punto_3.sql

--- punto_3.sql
+++ punto_3.sql
@@ -1,3 +1,9 @@
 SELECT employees.first_name, employees.last_name, employees.department_id, jobs.job_title,departments.department_name, locations.city
-FROM employees, jobs, departments, locations
-WHERE city ='London' 
+FROM employees
+INNER JOIN jobs
+ON employees.JOB_ID = jobs.JOB_ID 
+INNER JOIN departments
+ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
+INNER JOIN locations
+ON departments.LOCATION_ID = locations.LOCATION_ID
+WHERE locations.CITY ='London' 
