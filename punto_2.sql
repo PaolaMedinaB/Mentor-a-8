05f4ef7b7fb0c943d9eabfa6cc1bf5315840865f
Luig G. Moreno <luisgmore@gmail.com>
2019-03-13 20:24:30 -0500

    2

--- punto_2.sql
+++ punto_2.sql
@@ -3,3 +3,10 @@ SELECT employees.first_name, employees.last_name, employees.department_id, depar
 FROM employees
 INNER JOIN departments
 ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID	
+
+-- usar left join porque kimberly grant no tiene departamento valido
+
+SELECT employees.first_name, employees.last_name, employees.department_id, departments.department_name
+FROM employees
+LEFT JOIN departments
+ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID	
